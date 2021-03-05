4325f8538aa100fa3ec4dc35e65cceae4b48ec9f
Honanaipr <honanaipr@gmail.com>
2021-03-05 16:50:24 -0500

    WEB MWP

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
+@app.route('/encrypt.html')
+@app.route('/index')
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def encrypt():
+    return render_template('encrypt.html')
+
+
+@app.route('/decrypt.html')
+def decrypt():
+    return render_template('decrypt.html')
